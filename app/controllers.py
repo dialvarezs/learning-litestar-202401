1a65814992a6448b102968e77709f17b8c33f681
Diego Alvarez S. <dialvarezs@gmail.com>
2024-07-01 17:43:51 -0300

    Add repositores

--- app/controllers.py
+++ app/controllers.py
@@ -1,66 +1,62 @@
 from typing import Sequence
 
+from advanced_alchemy.filters import SearchFilter
 from litestar import Controller, delete, get, patch, post
 from litestar.dto import DTOData
 from litestar.exceptions import NotFoundException
-from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm import Session
 
 from app.dtos import TodoItemCreateDTO, TodoItemReadDTO, TodoItemUpdateDTO
 from app.models import TodoItem
+from app.repositories import TodoItemRepository, provide_todoitem_repo
 
 
 class ItemController(Controller):
     path = "/items"
+    dependencies = {"todoitem_repo": provide_todoitem_repo}
     return_dto = TodoItemReadDTO
 
     @get()
     async def get_items(
-        self, db_session: Session, listo: bool | None = None
+        self, todoitem_repo: TodoItemRepository, listo: bool | None = None
     ) -> Sequence[TodoItem]:
-        stmt = select(TodoItem)
-        if listo is not None:
-            stmt = stmt.where(TodoItem.listo == listo)
-
-        return db_session.execute(stmt).scalars().all()
+        if listo is None:
+            return todoitem_repo.list()
+        else:
+            return todoitem_repo.list(SearchFilter(field_name="listo", value="listo"))
 
     @get("/{item_id:int}")
-    async def get_item(self, db_session: Session, item_id: int) -> TodoItem:
+    async def get_item(
+        self, todoitem_repo: TodoItemRepository, item_id: int
+    ) -> TodoItem:
         try:
-            stmt = select(TodoItem).where(TodoItem.id == item_id)
-            return db_session.execute(stmt).scalar_one()
+            return todoitem_repo.get(item_id)
         except NoResultFound:
             raise NotFoundException(detail=f"Item {item_id} no encontrado")
 
     @post(dto=TodoItemCreateDTO)
-    async def add_item(self, db_session: Session, data: TodoItem) -> TodoItem:
-        db_session.add(data)
-        db_session.commit()
-
-        return data
+    async def add_item(
+        self, todoitem_repo: TodoItemRepository, data: TodoItem
+    ) -> TodoItem:
+        return todoitem_repo.add(data)
 
     @patch("/{item_id:int}", dto=TodoItemUpdateDTO)
     async def change_item(
-        self, db_session: Session, item_id: int, data: DTOData[TodoItem]
+        self, todoitem_repo: TodoItemRepository, item_id: int, data: DTOData[TodoItem]
     ) -> TodoItem:
         try:
-            stmt = select(TodoItem).where(TodoItem.id == item_id)
-            item = db_session.execute(stmt).scalar_one()
-
-            data.update_instance(item)
-            db_session.commit()
-
+            item, _ = todoitem_repo.get_and_update(
+                id=item_id, **data.as_builtins(), match_fields=["id"]
+            )
             return item
         except NoResultFound:
             raise NotFoundException(detail=f"Item {item_id} no encontrado")
 
     @delete("/{item_id:int}")
-    async def delete_item(self, db_session: Session, item_id: int) -> None:
+    async def delete_item(
+        self, todoitem_repo: TodoItemRepository, item_id: int
+    ) -> None:
         try:
-            stmt = select(TodoItem).where(TodoItem.id == item_id)
-            item = db_session.execute(stmt).scalar_one()
-            db_session.delete(item)
-            db_session.commit()
+            todoitem_repo.delete(item_id)
         except NoResultFound:
             raise NotFoundException(detail=f"Item {item_id} no encontrado")
---
+++ app/repositories.py
@@ -0,0 +1,12 @@
+from advanced_alchemy.repository import SQLAlchemySyncRepository
+from sqlalchemy.orm import Session
+
+from app.models import TodoItem
+
+
+class TodoItemRepository(SQLAlchemySyncRepository[TodoItem]): # type: ignore
+    model_type = TodoItem
+
+
+def provide_todoitem_repo(db_session: Session) -> TodoItemRepository:
+    return TodoItemRepository(session=db_session, auto_commit=True)
